853a83af4610ed59fb5b8f7be28e916335597702
João Brentano <joaobrentano.br@gmail.com>
2021-06-06 16:50:29 -0300

    v2

--- TecnoTinder/ViewController.swift
+++ TecnoTinder/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LoginEmailButton: UIButton!
+    @IBOutlet weak var LoginAppleButton: UIButton!
+    @IBOutlet weak var EmailField: UITextField!
+    @IBOutlet weak var SenhaField: UITextField!
+    @IBAction func LoginEmailButtonAction(_ sender: Any) {
+        if EmailField.hasText && SenhaField.hasText {
+            print("Login via e-mail e senha")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        LoginEmailButton.layer.cornerRadius = 20
+        LoginAppleButton.layer.cornerRadius = 20
     }
 
 
